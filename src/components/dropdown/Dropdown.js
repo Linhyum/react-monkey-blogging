41e81170839235fe25d476996f548aba6581b47a
Linhyum <phiep860974@gmail.com>
2023-09-16 10:05:11 +0700

    fix bug

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { DropdownProvider, useDropdown } from "./dropdown-context";
+import { useDropdown } from "./dropdown-context";
 import Select from "./Select";
 import Content from "./Content";
 
 const Dropdown = ({ placeholder = "Please select an option", children, ...props }) => {
-    const { nodeRef } = useDropdown();
+   const { nodeRef } = useDropdown();
 
-    return (
-        <div ref={nodeRef} className="relative text-[#333] z-10 inline-block w-full">
-            <Select placeholder={placeholder}></Select>
-            <Content>{children}</Content>
-        </div>
-    );
+   return (
+      <div ref={nodeRef} className="relative text-[#333] z-10 inline-block w-full">
+         <Select placeholder={placeholder}></Select>
+         <Content>{children}</Content>
+      </div>
+   );
 };
 
 export default Dropdown;
--- src/module/dashboard/DashboardLayout.js
+++ src/module/dashboard/DashboardLayout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import DashboardHeader from "./DashboardHeader";
@@ -6,48 +6,48 @@ import Sidebar from "./Sidebar";
 import { useAuth } from "../../contexts/auth-context";
 
 const DashboardStyles = styled.div`
-    max-width: 1600px;
-    margin: 0 auto;
-    .dashboard {
-        &-heading {
-            font-weight: bold;
-            font-size: 36px;
-            /* margin-bottom: 40px; */
-            color: ${(props) => props.theme.primary};
-            letter-spacing: 1px;
-        }
-        &-main {
-            display: grid;
-            padding: 40px 20px;
-            gap: 0 40px;
-            align-items: start;
-        }
-    }
-    @media screen and (min-width: 1280px) {
-        .dashboard-main {
-            grid-template-columns: 280px minmax(0, 1fr);
-        }
-    }
+   max-width: 1600px;
+   margin: 0 auto;
+   .dashboard {
+      &-heading {
+         font-weight: bold;
+         font-size: 36px;
+         /* margin-bottom: 40px; */
+         color: ${(props) => props.theme.primary};
+         letter-spacing: 1px;
+      }
+      &-main {
+         display: grid;
+         padding: 40px 20px;
+         gap: 0 40px;
+         align-items: start;
+      }
+   }
+   @media screen and (min-width: 1280px) {
+      .dashboard-main {
+         grid-template-columns: 280px minmax(0, 1fr);
+      }
+   }
 `;
 const DashboardLayout = ({ children }) => {
-    const { userInfo } = useAuth();
-    const navigate = useNavigate();
-    //nếu chưa đăng nhập thì k vô được Dashboard
-    if (!userInfo) {
-        navigate("/sign-in");
-    }
+   const { userInfo } = useAuth();
+   const navigate = useNavigate();
+   //nếu chưa đăng nhập thì k vô được Dashboard
+   if (!userInfo) {
+      navigate("/sign-in");
+   }
 
-    return (
-        <DashboardStyles>
-            <DashboardHeader></DashboardHeader>
-            <div className="grid-cols-1 dashboard-main">
-                <Sidebar></Sidebar>
-                <div className="dashboard-children">
-                    <Outlet></Outlet>
-                </div>
+   return (
+      <DashboardStyles>
+         <DashboardHeader></DashboardHeader>
+         <div className="grid-cols-1 dashboard-main">
+            <Sidebar></Sidebar>
+            <div className="dashboard-children">
+               <Outlet></Outlet>
             </div>
-        </DashboardStyles>
-    );
+         </div>
+      </DashboardStyles>
+   );
 };
 
 export default DashboardLayout;
